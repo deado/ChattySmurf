5ac35ab1b82863b74ec0d6db0325cbdbead24e34
Jeremy Polley <deado@irohe.com>
2017-01-24 05:24:36 -0600

    Hello World!

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/* ChattySmurf!
+IRC Client written in cpp by deado.
+It sucks and probably will never be 100% completed.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout<<"Hello World!\n";
+  return 0;
+}
+
