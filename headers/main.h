0548e1746980541d6887fc4e372cfc3014d6e844
Jeremy Polley <deado@irohe.com>
2017-01-24 12:40:37 -0600

    created new header file with a test function and custom classes and new starting point for actual program.

---
+++ headers/main.h
@@ -0,0 +1,43 @@
+#include <iostream> //standard i/o streaming to stdio c lib
+#include <unistd.h> //POSIX; getopt and much much more =D
+#include <string> //dont think this is included in iostream...
+
+using namespace std; //apparently after I stopped C++ namespaces took over...
+
+
+//created custom class
+/* Client class is used for all local user information to be stored during runtime. */
+class Client {
+//public variables
+  public:
+    string username;
+    string ident;
+    string fullname;
+    string email;
+    string hostname;
+    string ip;
+};
+
+/* DCC Class is going to be used for Direct Client-to-Client transfers */
+class Dcc {
+  public:
+    int port;
+    string hostname;
+};
+
+/* Server Class; Server and connection information variables */
+class Server {
+  public:
+    int port;
+    string host;
+    string channel[];
+    bool issecure;
+};
+
+/* Custom funtion; show working inside header files; no use yet */
+int test()
+{
+  cout<<"Made it into the header file!\n";
+  return(0);
+}
+
--- main.cpp
+++ main.cpp
@@ -2,14 +2,16 @@
 IRC Client written in cpp by deado.
 It sucks and probably will never be 100% completed.
 */
-
-#include <iostream>
-
-using namespace std;
+#include "headers/main.h" //include custom header file with the fun stuff...
 
 int main()
 {
-  cout<<"Hello World!\n";
+  cout<<"Hello World!\n"; //standard output
+  test(); //call custom function
+
+  Client foo; //define foo New Client;
+  foo.username = "deado"; //set username
+  cout << foo.username << endl; //output and flush
   return 0;
 }
 
